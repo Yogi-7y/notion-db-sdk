71e61dd4954a01c745f0a8b91c1775a0f6ded558
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-07-23 12:27:05 +0530

    feat(property): introduce date property

---
+++ package/lib/src/module/domain/entity/property_variants/date.dart
@@ -0,0 +1,22 @@
+import 'package:meta/meta.dart';
+
+import '../property.dart';
+
+@immutable
+class Date extends Property<DateTime> {
+  const Date({
+    required super.name,
+    required super.type,
+    super.id,
+    super.valueDetails,
+  });
+
+  String get _formattedDate => value?.toIso8601String().split('T').first ?? '';
+
+  @override
+  Map<String, Object?> toMap() => {
+        name: {
+          'date': _formattedDate,
+        }
+      };
+}
---
+++ package/test/src/module/domain/entity/property_variants/date_test.dart
@@ -0,0 +1,28 @@
+import 'package:package/src/module/domain/entity/property_variants/date.dart';
+import 'package:package/src/module/domain/entity/value.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+    'date property successfully generates a map as per the contract',
+    () {
+      final _expectedMap = <String, Object?>{
+        'Date': {
+          'date': '2021-09-01',
+        }
+      };
+
+      final _date = Date(
+        name: 'Date',
+        type: 'date',
+        valueDetails: Value(
+          value: DateTime(2021, 9),
+        ),
+      );
+
+      final _map = _date.toMap();
+
+      expect(_map, _expectedMap);
+    },
+  );
+}
