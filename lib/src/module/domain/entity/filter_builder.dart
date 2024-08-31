57dd9f879777c64de15aca52ee3ebc8622cda254
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-09-01 00:56:43 +0530

    feat(filter): filter builder

---
+++ lib/src/module/domain/entity/filter_builder.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: avoid_returning_this
+
+import 'filter.dart';
+
+class FilterBuilder {
+  Filter? _filter;
+
+  FilterBuilder and(List<Filter> filters) {
+    _filter = AndFilter(filters);
+    return this;
+  }
+
+  Filter build() {
+    if (_filter == null) {
+      throw StateError('No filter has been set. Use and() or or() to set a filter.');
+    }
+    return _filter!;
+  }
+
+  FilterBuilder or(List<Filter> filters) {
+    _filter = OrFilter(filters);
+    return this;
+  }
+}
